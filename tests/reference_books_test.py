285d7b9442d27ed4118112256ab0c603b4002a3e
Dmitry <khilkodmitry@mail.ru>
2022-11-25 22:05:23 +0300

    Создан чек лист

--- tests/reference_books_test.py
+++ tests/reference_books_test.py
@@ -1,39 +1,37 @@
 import allure
 
 
-@allure.label("modul", "Администрирование")
+@allure.label("owner", "khilko")
 @allure.label("layer", "ui")
+@allure.label("module", "Администрирование")
+@allure.label("microservice", "referenceBooks")
 class TestReferenceBooks:
 
-    @allure.id("142")
-    @allure.label("owner", "khilko")
     @allure.manual
-    @allure.label("submodul", "Справочник оплат")
+    @allure.label("submodule", "Справочник оплат")
     @allure.feature("Создание новой позиции справочника оплат")
-    @allure.label("microservice", "referenceBooks")
     @allure.tag("pozitive", "smoke")
     @allure.link('https://bitrix.ivcmf.by/knowledge/SPB/referenceBooksPayment', '', 'Справочник оплат')
     @allure.title("Создание новой позиции справочника оплат (название: валидное)")
-    def test_ReferenceBooksPaymentAddValidAll(self):
-        with allure.step("Перейти на страницу 'Справочник оплат'"):
-            pass
-        with allure.step("Нажать кнопку 'Добавить'"):
-            with allure.step("Ожидаем, что отобразилось диалоговое окно 'Добавление позиции в справочник оплат'"):
-                pass
-        with allure.step("Ввести в поле 'Наименование' наименование новой позиции"):
-            pass
-        with allure.step("Нажать кнопку 'Ок'"):
-            with allure.step("Ожидаем, что новая позиция справочника отобразилась в таблице на странице 'Справочник оплат'"):
-                pass
+    def test_reference_books_payment_add_valid_all(self):
+        pass
 
-    @allure.id("141")
-    @allure.label("owner", "khilko")
     @allure.manual
-    @allure.label("submodul", "Справочник оплат")
+    @allure.label("submodule", "Справочник оплат")
     @allure.feature("Создание новой позиции справочника оплат")
-    @allure.label("microservice", "referenceBooks")
     @allure.tag("negative")
     @allure.link('https://bitrix.ivcmf.by/knowledge/SPB/referenceBooksPayment', '', 'Справочник оплат')
-    @allure.title("Создание новой позиции справочника оплат (~название: пустое)")
-    def test_ReferenceBooksPaymentAddEmptyTitle(self):
+    @allure.title("Создание новой позиции справочника оплат (~название: пусто)")
+    def test_reference_books_payment_add_empty_title(self):
         pass
+
+    @allure.manual
+    @allure.label("submodule", "Справочник оплат")
+    @allure.feature("Создание новой позиции справочника оплат")
+    @allure.tag("negative")
+    @allure.link('https://bitrix.ivcmf.by/knowledge/SPB/referenceBooksPayment', '', 'Справочник оплат')
+    @allure.title("Создание новой позиции справочника оплат (~название: больше 250 символов)")
+    def test_reference_books_payment_add_big_title(self):
+        pass
+
+
